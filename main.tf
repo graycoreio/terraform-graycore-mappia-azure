b43d8ca400acb1e549c30289b3ab33413f17be44
GrayBot <120588553+gray-bot@users.noreply.github.com>
2023-05-26 11:21:28 -0400

    chore: release 0.3.3 (#596)

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "helm_release" "ingress" {
 
 module "mappia" {
   source = "../mappia"
-  # version = "0.3.2" # x-release-please-version
+  # version = "0.3.3" # x-release-please-version
   depends_on = [
     helm_release.ingress
   ]
