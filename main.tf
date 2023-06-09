695ab51d0eddbe4d1761e64a43231af9a54a9f25
Damien Retzinger <damienwebdev@gmail.com>
2023-06-09 10:26:07 -0400

    test: fix test for node count

--- main.tf
+++ main.tf
@@ -67,8 +67,8 @@ resource "helm_release" "ingress" {
 }
 
 module "mappia" {
-  source  = "app.terraform.io/graycore/mappia/graycore"
-  version = "0.3.4" # x-release-please-version
+  source = "../mappia"
+  # version = "0.3.4" # x-release-please-version
   depends_on = [
     helm_release.ingress
   ]
