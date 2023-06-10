766c89984a38dee0ae179e51b7905de0a7f3bbd3
GrayBot <120588553+gray-bot@users.noreply.github.com>
2023-06-10 11:15:42 -0400

    chore: release 0.3.7-alpha.1 (#610)

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "helm_release" "ingress" {
 
 module "mappia" {
   source  = "graycoreio/mappia/graycore"
-  version = "0.3.7-alpha.0" # x-release-please-version
+  version = "0.3.7-alpha.1" # x-release-please-version
   depends_on = [
     helm_release.ingress
   ]
